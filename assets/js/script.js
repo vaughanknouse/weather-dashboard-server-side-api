f2a0716807c6532104ad80ac770dc001ad1c01c8
Vaughan Knouse <vaughanknouse@gmail.com>
2024-05-06 19:25:24 -0400

    updated README and index.HTML

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// OpenWeather API key
+const APIKey = 'f5719b4222054bee24b27c3877c4bc7a';
+
+// Collect user input for just the city name
+// let city; 
+// OpenWeather Current Weather Data URL and necessary variables
+// const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+
+// Base URL:
+const queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey}';
