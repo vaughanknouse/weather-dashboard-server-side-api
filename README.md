ec68425c2b72f2c442fd0bfe1edbfcdb12abb22f
Vaughan Knouse <vaughanknouse@gmail.com>
2024-05-10 23:37:31 -0400

    updated README, javascript, CSS, and HMTL

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 ## Description
 
-This week's challenge involves ***
+This week's challenge involves using the skills and knowledge gained over the past several weeks of the UT Austin Coding Boot Camp course to build a weather dashboard application from scratch that runs in the browser using HTML, CSS, as well as JavaScript. Using the sever-side API, OpenWeather API, weather data was retrieved for cities by making requests with specific parameters to a URL and that data was then used in the context of this weather dashboard application.
+
+This weather dashboard displays up-to-date current and future weather data for a city entered by a user. Viewing the current weather conditions and a five-day forecast for a specific city reveals weather details including the city name, the date, a weather icon representing the weather condition, as well as the temperature in Fahrenheit, humidity percentage, and wind speed in miles per hour. After the city is entered and the weather data is displayed on the dashboard, each city is also saved on the webpage and can be accessed quickly once again by clicking on the city name to reveal the weather data for that city. Additionally, the cities entered are saved in local storage so that they remain on the page if the page is closed or refreshed.
+
+This weather dashboard application enables individuals, most notably those who are traveling to multiple cities and need to plan their trips accordingly, to easily access and view the current and future weather conditions of those cities. Through completing this challenge, I was able to improve my understanding of the workings of server-side APIs and how they are beneficial in helping developers access and retrieve data from an application developed by another individual or company in order to apply that retrieved data to a current project.
+
 
 ## User Story
 
@@ -26,11 +31,9 @@ THEN I am again presented with current and future conditions for that city
 ```
 
 ## Mock Up
-The following images show the web application's appearance: ***
+The following image shows the web application's appearance:
 
-![Shows depoyed web application with webpage heading and developer's photo.](assets/images/advanced-css-portfolio.png)
-
-![Shows depoyed web application with webpage heading and developer's photo.](assets/images/advanced-css-portfolio.png)
+![Shows deployed weather dashboard application with a page that includes a form input to search for a city, the list of searched cities, the current weather conditions for that city, and the 5-day forecast for that city.](assets/images/weather-dashboard-screenshot.png)
 
 ## Link to Deployed Application
 
@@ -39,8 +42,11 @@ The following images show the web application's appearance: ***
 ## Credits
 Used the following sources as tutorials and guidelines: ***
 
+The OpenWeatherMap API: https://openweathermap.org/forecast5.
+
 Ninja Net's Bootstrap 5 Crash Course Tutorials: https://www.youtube.com/playlist?list=PL4cUxeGkcC9joIM91nLzd_qaH_AimmdAR
 
+"How to Use API Keys" article: https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys/ 
 
 Also utilized the Xpert Learning assistant for some portions of my code:
 https://bootcampspot.instructure.com/courses/5293/external_tools/313
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,174 @@
-// Collect user input for just the city name
-// let city; 
-// OpenWeather Current Weather Data URL and necessary variables
-// const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-
-// Base URL:
-// const queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey}';
-
-
-
-
-
 // OpenWeather API key
-const apiKey = 'f5719b4222054bee24b27c3877c4bc7a';
-const cityInput = document.querySelector("#search-city");
-const searchButton = document.querySelector("#search-button");
+const apiKey = "f5719b4222054bee24b27c3877c4bc7a";
 
-const getWeatherDetails = function(cityName, lat, lon) {
-  const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+// Query Selectors
+const cityFormEl = document.querySelector('#city-form');
+const cityInputEl = document.querySelector("#search-city");
+const searchEl = document.querySelector("#search-button");
+const historyEl = document.querySelector("#history");
+const cityNameEl = document.querySelector("#name");
+const dateEl = document.querySelector("#date");
+const iconEl = document.querySelector("#icon");
+const tempEl = document.querySelector("#temp");
+const humidityEl = document.querySelector("#humidity");
+const windEl = document.querySelector("#wind");
+const forecastEl = document.querySelector("#forecast-body");
+const currentCityContEl = document.querySelector("#current-city-container");
+const forecastContEl = document.querySelector("#forecast-container");
+const todayWeatherEl = document.querySelector("#today-weather");
 
-  fetch(WEATHER_API_URL)
-    .then (function (response) {
-      return response.json();
-    })
-    .then (function (data) {
-      console.log(data);
-    })
-    .catch(function (error) {
-      alert("An error occured while fetching the weather forecast!");
-    });
+// Current weather stored within the object currentWeather
+const currentWeather = {
+  name:"",
+  date:"",
+  temp:"",
+  humidity:"",
+  wind:"",
+  icon:""
 }
 
-// Function to get coordinates of an entered city
-const getCityCoordinates = function() {
-  const cityName = cityInput.value.trim(); // Get user entered city name and remove extra space
-  if (!cityName) return; // Return if cityName is empty
-  console.log(cityName);
+//define searchCity variable
+const searchCity = cityInputEl.value.trim();
 
-  const GEOCODING_API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+// Store the forecast data as array
+let forecast = [];
 
-  // Get entered city coordinates (latitude, longitude, and name) from the API response
-  fetch(GEOCODING_API_URL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      if(!data.length) {
-        return alert(`No coordinates found for ${cityName}`);
+// use Day.js for dates
+const today = dayjs();
 
-      const { name, lat, lon } = data[0];
-      getWeatherDetails(name, lat, lon);
-     }
-    }).catch(function (error) {
-      alert("An error occured while fetching the coordinates!");
-    });
+// Array to store search history and retrieve from local Storage or initalize as empty array
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+
+// getWeather function to get weather data and also display the latitude and longitude coordinates
+const getWeather = function (city) {
+  const apicityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  let lat = "";
+  let lon = "";
+
+  fetch(apicityUrl).then(function(response) {
+    if(response.ok) {
+      response.json().then(function(data) {
+        //console.log(data);
+        currentWeather.name = data.name + " ";
+        currentWeather.date = today.format("MM/DD/YYYY");
+        currentWeather.temp = data.main.temp + " &#176F";
+        currentWeather.humidity = data.main.humidity+"%";
+        currentWeather.wind = data.wind.speed + " MPH";
+        currentWeather.icon = data.weather[0].icon;
+        //console.log(currentWeather.icon);
+        lat = data.coord.lat;
+        lon = data.coord.lon;
+
+        const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+        fetch(apiUrl)
+        .then(function(coordresponse) {
+          if (coordresponse.ok) {
+            coordresponse.json().then(function(coordData) {
+              //console.log(coordData);
+              console.log(`Coordinates for ${city}: Latitude - ${lat}, Longitude - ${lon}`);
+              displayWeather();
+              //getForecast(city);
+            })
+          }
+
+        }) .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+
+      });
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching coordinates:', error);
+    alert('Error fetching coordinates. Please try again.');
+  });
+}
+
+
+// display collected information from OpenWeather API on the page
+const displayWeather = function (data) {
+  todayWeatherEl.style.display = "block";
+  forecastContEl.style.display = "block";
+  cityNameEl.innerHTML = currentWeather.name;
+  dateEl.innerHTML = currentWeather.date;
+  tempEl.innerHTML = currentWeather.temp;
+  humidityEl.innerHTML = currentWeather.humidity;
+  windEl.innerHTML = currentWeather.wind;
+  iconEl.innerHTML = "<img src='https://openweathermap.org/img/wn/" + currentWeather.icon + "@2x.png'></img>";
 };
 
-// eventListener for search button
-searchButton.addEventListener("click", getCityCoordinates);
+// getForecast function to get 5-day forecast data
+
+// displayForecast function to display 5-day forecast data
+
+// function to display search history on the page
+const displayHistory = function() {
+  historyEl.innerHTML = "";
+  for (let i = 0; i < searchHistory.length; i++) {
+    const historyDiv = document.createElement("div");
+    historyDiv.classList.add("history-item");
+    historyDiv.innerHTML = "<h4>"+searchHistory[i]+"<h4>";
+    historyEl.appendChild(historyDiv);
+  }
+}
+
+// function to load search history from local Storage and sends it to display history function
+const loadHistory = function() {
+  searchHistory = JSON.parse(localStorage.getItem("history"));
+  if (!searchHistory) {
+    searchHistory = [];
+  }
+  displayHistory();
+}
 
 
+// clear forecast data from page and empty forecast array
+const clearForecast = function() {
+  forecast = [];
+  forecastEl.innerHTML = "";
+}
 
+// historyClickHandler function called by event listener when click on the particular city in the search history
+const historyClickHandler = function (event) {
+  const historyCity = event.target.textContent;
+  if (historyCity) {
+    clearForecast();
+    getWeather(historyCity);
+  }
+}
 
+// formSubmitHandler function called by event listener when click on the search button, which gets value from form input and sends it to getWeather function
+const formSubmitHandler = function(event) {
+  event.preventDefault();
+
+  const searchCity = cityInputEl.value.trim();
+  if (searchCity) {
+    getWeather(searchCity);
+    // Update search history and local storage
+    searchHistory.push(searchCity);
+    localStorage.removeItem("history");
+    localStorage.setItem("history", JSON.stringify(searchHistory));
+    clearForecast();
+    displayHistory();
+    cityInputEl.value ="";
+   }
+  else {
+    return;
+  }
+}
+
+// clear data from results and hide current as well as future weather data
+const clearResults = function () {
+console.log("inside clearResults");
+todayWeatherEl.style.display = "none";
+forecastContEl.style.display = "none";
+dateEl.innerHTML = "";
+iconEl.html = "";
+}
+
+loadHistory();
+
+// Event listeners for when user clicks Search button on the form or clicks on city search history name
+cityFormEl.addEventListener('submit', formSubmitHandler);
+historyEl.addEventListener('click', historyClickHandler);
